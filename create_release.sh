8cec3fb80724de7e766ddc02fb27d48420ac4893
Kenneth <kensoares@gmail.com>
2022-12-10 16:03:18 +0100

    automation of tarball creation

---
+++ create_release.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+ver=0.0.1
+mkdir -p releases/MiniJPad-$ver
+cp -r MiniJPad/ releases/MiniJPad-$ver
+cp MiniJPad.desktop releases/MiniJPad-$ver
+cp install.sh releases/MiniJPad-$ver
+tar -czvf releases/MiniJPad-$ver.tar.gz releases/MiniJPad-$ver
+rm -rf releases/MiniJPad-$ver
