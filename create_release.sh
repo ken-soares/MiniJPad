383451c057472bea370547a4d2564a3f8a78b1fc
Kenneth <kensoares@gmail.com>
2022-12-10 16:04:19 +0100

    added LICENCE and README

--- create_release.sh
+++ create_release.sh
@@ -5,5 +5,7 @@ mkdir -p releases/MiniJPad-$ver
 cp -r MiniJPad/ releases/MiniJPad-$ver
 cp MiniJPad.desktop releases/MiniJPad-$ver
 cp install.sh releases/MiniJPad-$ver
+cp LICENCE releases/MiniJPad-$ver
+cp README.md releases/MiniJPad-$ver
 tar -czvf releases/MiniJPad-$ver.tar.gz releases/MiniJPad-$ver
 rm -rf releases/MiniJPad-$ver
